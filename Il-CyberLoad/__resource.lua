498b4db6e3f7abb12a5ddedb85f3d32c9cbf7b8c
Kentain_fr <63867494+Kentainfr@users.noreply.github.com>
2020-11-26 16:04:57 +0100

    Add files via upload

---
+++ Il-CyberLoad/__resource.lua
@@ -0,0 +1,10 @@
+files {
+    'index.html',
+    'css/style.css',
+}
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+client_script 'client.lua'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
